5f4ba0d6990a7086af15c2cb55bbb9b7ac69139c
saviour123 <profsaviour@gmail.com>
2016-07-14 22:45:04 +0000

    Second Commit

--- route.py
+++ route.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/generic')
+def generic():
+	return render_template('generic.html')
+
+@app.route('/elements')
+def elements():
+	return render_template('elements.html')
+
 
 
 if __name__ == '__main__':
